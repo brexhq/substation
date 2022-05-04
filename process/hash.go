91baa8276fa66f4e818c274a6034abbc16be1998
jshlbrd <jliburdi@brex.com>
2022-05-04 20:55:30 +0000

    feat: add new processing patterns to Hash

--- process/hash.go
+++ process/hash.go
@@ -7,19 +7,53 @@ import (
 	"fmt"
 
 	"github.com/brexhq/substation/condition"
+	"github.com/brexhq/substation/internal/errors"
 	"github.com/brexhq/substation/internal/json"
 )
 
-/*
-HashOptions contain custom options settings for this processor.
+// HashInvalidSettings is returned when the Hash processor is configured with invalid Input and Output settings.
+const HashInvalidSettings = errors.Error("HashInvalidSettings")
 
-Algorithm: the algorithm to apply.
+// HashUnsupportedAlgorithm is returned when the Hash processor is configured with an unsupported algorithm.
+const HashUnsupportedAlgorithm = errors.Error("HashUnsupportedAlgorithm")
+
+/*
+HashOptions contains custom options for the Hash processor:
+	Algorithm:
+		the hashing algorithm to apply
+		must be one of:
+			md5
+			sha256
 */
 type HashOptions struct {
 	Algorithm string `mapstructure:"algorithm"`
 }
 
-// Hash implements the Byter and Channeler interfaces and calculates the hash of data. More information is available in the README.
+/*
+Hash processes data by calculating hashes. The processor supports these patterns:
+	json:
+		{"hash":"foo"} >>> {"hash":"acbd18db4cc2f85cedef654fccc4a4d8"}
+	json array:
+		{"hash":["foo","bar"]} >>> {"hash":["acbd18db4cc2f85cedef654fccc4a4d8","37b51d194a7513e45b56f6524f2d51f2"]}
+	data:
+		foo >>> acbd18db4cc2f85cedef654fccc4a4d8
+
+The processor uses this Jsonnet configuration:
+	{
+		type: 'hash',
+		settings: {
+			input: {
+				key: 'hash',
+			},
+			output: {
+				key: 'hash',
+			}
+			options: {
+				algorithm: 'md5',
+			}
+		},
+	}
+*/
 type Hash struct {
 	Condition condition.OperatorConfig `mapstructure:"condition"`
 	Input     Input                    `mapstructure:"input"`
@@ -27,7 +61,7 @@ type Hash struct {
 	Options   HashOptions              `mapstructure:"options"`
 }
 
-// Channel processes a data channel of bytes with this processor. Conditions can be optionally applied on the channel data to enable processing.
+// Channel processes a data channel of byte slices with the Hash processor. Conditions are optionally applied on the channel data to enable processing.
 func (p Hash) Channel(ctx context.Context, ch <-chan []byte) (<-chan []byte, error) {
 	var array [][]byte
 
@@ -63,35 +97,57 @@ func (p Hash) Channel(ctx context.Context, ch <-chan []byte) (<-chan []byte, err
 
 }
 
-// Byte processes a byte slice with this processor
+// Byte processes a byte slice with the Hash processor.
 func (p Hash) Byte(ctx context.Context, data []byte) ([]byte, error) {
-	value := json.Get(data, p.Input.Key)
+	// json processing
+	if p.Input.Key != "" && p.Output.Key != "" {
+		value := json.Get(data, p.Input.Key)
+		if !value.IsArray() {
+			b := []byte(value.String())
+			h, err := p.hash(b)
+			if err != nil {
+				return nil, err
+			}
 
-	if !value.IsArray() {
-		b := []byte(value.String())
-		o := p.hash(b)
-		return json.Set(data, p.Output.Key, o)
+			return json.Set(data, p.Output.Key, h)
+		}
+
+		// json array processing
+		var array []string
+		for _, v := range value.Array() {
+			b := []byte(v.String())
+			h, err := p.hash(b)
+			if err != nil {
+				return nil, err
+			}
+
+			array = append(array, h)
+		}
+
+		return json.Set(data, p.Output.Key, array)
 	}
 
-	var array []string
-	for _, v := range value.Array() {
-		b := []byte(v.String())
-		o := p.hash(b)
-		array = append(array, o)
+	// data processing
+	if p.Input.Key == "" && p.Output.Key == "" {
+		h, err := p.hash(data)
+		if err != nil {
+			return nil, err
+		}
+		return []byte(h), nil
 	}
 
-	return json.Set(data, p.Output.Key, array)
+	return nil, HashInvalidSettings
 }
 
-func (p Hash) hash(b []byte) string {
+func (p Hash) hash(b []byte) (string, error) {
 	switch a := p.Options.Algorithm; a {
 	case "md5":
 		sum := md5.Sum(b)
-		return fmt.Sprintf("%x", sum)
+		return fmt.Sprintf("%x", sum), nil
 	case "sha256":
 		sum := sha256.Sum256(b)
-		return fmt.Sprintf("%x", sum)
+		return fmt.Sprintf("%x", sum), nil
 	default:
-		return ""
+		return "", HashUnsupportedAlgorithm
 	}
 }

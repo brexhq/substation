fea0d5e585bdace6f2e7d1e7e6786ceb4c971af9
jshlbrd <jliburdi@brex.com>
2022-05-04 00:27:27 +0000

    test: add benchmarks to Count

--- process/count_test.go
+++ process/count_test.go
@@ -6,26 +6,29 @@ import (
 	"testing"
 )
 
-func TestCount(t *testing.T) {
-	var tests = []struct {
-		proc     Count
-		test     [][]byte
-		expected []byte
-	}{
-		{
-			Count{},
-			[][]byte{
-				[]byte(`{"foo":"bar"}`),
-				[]byte(`{"foo":"baz"}`),
-				[]byte(`{"foo":"qux"}`),
-			},
-			[]byte(`{"count":3}`),
+var countTests = []struct {
+	name     string
+	proc     Count
+	test     [][]byte
+	expected []byte
+}{
+	{
+		"count",
+		Count{},
+		[][]byte{
+			[]byte(`{"foo":"bar"}`),
+			[]byte(`{"foo":"baz"}`),
+			[]byte(`{"foo":"qux"}`),
 		},
-	}
+		[]byte(`{"count":3}`),
+	},
+}
+
+func TestCount(t *testing.T) {
 
 	ctx := context.TODO()
 
-	for _, test := range tests {
+	for _, test := range countTests {
 		pipe := make(chan []byte, len(test.test))
 		for _, x := range test.test {
 			pipe <- x
@@ -47,5 +50,28 @@ func TestCount(t *testing.T) {
 			t.Logf("expected %s, got %s", test.expected, processed)
 			t.Fail()
 		}
+	}
+}
+
+func benchmarkCountByte(b *testing.B, byter Count, test chan []byte) {
+	ctx := context.TODO()
+	for i := 0; i < b.N; i++ {
+		byter.Channel(ctx, test)
+	}
+}
+
+func BenchmarkCountByte(b *testing.B) {
+	for _, test := range countTests {
+		pipe := make(chan []byte, len(test.test))
+		for _, x := range test.test {
+			pipe <- x
+		}
+		close(pipe)
+
+		b.Run(string(test.name),
+			func(b *testing.B) {
+				benchmarkCountByte(b, test.proc, pipe)
+			},
+		)
 	}
 }

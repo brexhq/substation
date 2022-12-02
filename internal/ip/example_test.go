d959fc3fb37b17fd339e74ef608495b66677d45f
Josh Liburdi <liburdi.joshua@gmail.com>
2022-12-02 16:10:30 +0000

    test: comment out setenv

--- internal/ip/example_test.go
+++ internal/ip/example_test.go
@@ -4,16 +4,13 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"os"
 
 	"github.com/brexhq/substation/internal/ip"
 )
 
 func Example_iP2Location() {
 	// the location of the IP enrichment database must be provided by environment variable and can be either a path on local disk, an HTTP(S) URL, or an AWS S3 URL
-	//nolint:tenv // example doesn't use testing package
-	_ = os.Setenv("IP2LOCATION_DB", "location://path/to/ip2location.bin")
-	defer os.Unsetenv("IP2LOCATION_DB")
+	// _ = os.Setenv("IP2LOCATION_DB", "location://path/to/ip2location.bin")
 
 	// create IP2Location container, load database, and close database when function returns
 	ip2loc := ip.IP2Location{}
@@ -38,9 +35,7 @@ func Example_iP2Location() {
 
 func Example_maxMind() {
 	// the location of the IP enrichment database must be provided by environment variable and can be either a path on local disk, an HTTP(S) URL, or an AWS S3 URL
-	//nolint:tenv // example doesn't use testing package
-	_ = os.Setenv("MAXMIND_LOCATION_DB", "location://path/to/maxmind.mmdb")
-	defer os.Unsetenv("MAXMIND_LOCATION_DB")
+	// _ = os.Setenv("MAXMIND_LOCATION_DB", "location://path/to/maxmind.mmdb")
 
 	// create MaxMind container, load database, and close database when function returns
 	mm := ip.MaxMind{}
--- process/example_test.go
+++ process/example_test.go
@@ -3,7 +3,6 @@ package process_test
 import (
 	"context"
 	"fmt"
-	"os"
 
 	"github.com/brexhq/substation/config"
 	"github.com/brexhq/substation/process"
@@ -14,13 +13,8 @@ func Example_iPInfo() {
 	capsule.SetData([]byte(`{"ip":"8.8.8.8"}`))
 
 	// the location of the IP enrichment database must be provided by environment variable and can be either a path on local disk, an HTTP(S) URL, or an AWS S3 URL
-	//nolint:tenv // example doesn't use testing package
-	_ = os.Setenv("MAXMIND_ASN_DB", "location://path/to/maxmind.mmdb")
-	defer os.Unsetenv("MAXMIND_ASN_DB")
-
-	//nolint:tenv // example doesn't use testing package
-	_ = os.Setenv("MAXMIND_LOCATION_DB", "location://path/to/maxmind.mmdb")
-	defer os.Unsetenv("MAXMIND_LOCATION_DB")
+	// _ = os.Setenv("MAXMIND_ASN_DB", "location://path/to/maxmind.mmdb")
+	// _ = os.Setenv("MAXMIND_LOCATION_DB", "location://path/to/maxmind.mmdb")
 
 	// in native Substation applications configuration is handled by compiling Jsonnet and loading JSON into the application
 	cfg := []config.Config{

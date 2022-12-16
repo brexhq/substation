7fb204e27f03a2749e1501dc85a10af24cfa2a9f
Josh Liburdi <liburdi.joshua@gmail.com>
2022-12-16 19:20:05 +0000

    fix: add rwmutex

--- internal/ip/database/ip2location.go
+++ internal/ip/database/ip2location.go
@@ -3,6 +3,7 @@ package database
 import (
 	"context"
 	"os"
+	"sync"
 
 	"github.com/brexhq/substation/internal/file"
 	"github.com/brexhq/substation/internal/ip"
@@ -12,6 +13,7 @@ import (
 // IP2Location provides read access to an IP2Location binary database.
 type IP2Location struct {
 	Database string `json:"database"`
+	mu       sync.RWMutex
 	db       *ip2location.DB
 }
 
@@ -22,6 +24,14 @@ func (d *IP2Location) IsEnabled() bool {
 
 // Open retrieves the database and opens it for querying. The location of the database can be either a path on local disk, an HTTP(S) URL, or an AWS S3 URL.
 func (d *IP2Location) Open(ctx context.Context) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	// avoids unnecessary opening
+	if d.db != nil {
+		return nil
+	}
+
 	path, err := file.Get(ctx, d.Database)
 	defer os.Remove(path)
 
@@ -38,15 +48,27 @@ func (d *IP2Location) Open(ctx context.Context) error {
 
 // Close closes the open database.
 func (d *IP2Location) Close() error {
-	if d.IsEnabled() {
-		d.db.Close()
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	// avoids unnecessary closing
+	if d.db == nil {
+		return nil
 	}
 
+	d.db.Close()
+
+	// db is made nil so that IsEnabled correctly
+	// returns the non-enabled state
+	d.db = nil
 	return nil
 }
 
 // Get queries the database and returns an aggregated database record containing enrichment information.
 func (d *IP2Location) Get(addr string) (*ip.EnrichmentRecord, error) {
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+
 	resp, err := d.db.Get_all(addr)
 	if err != nil {
 		return nil, err
--- internal/ip/database/maxmind.go
+++ internal/ip/database/maxmind.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"sync"
 
 	"github.com/brexhq/substation/internal/file"
 	"github.com/brexhq/substation/internal/ip"
@@ -15,16 +16,25 @@ import (
 type MaxMindASN struct {
 	Database string `json:"database"`
 	Language string `json:"language"`
-	db       *geoip2.Reader
+	mu       sync.RWMutex
+	reader   *geoip2.Reader
 }
 
 // IsEnabled returns true if the database is open and ready for use.
 func (d *MaxMindASN) IsEnabled() bool {
-	return d.db != nil
+	return d.reader != nil
 }
 
 // Open retrieves the database and opens it for querying. The location of the database can be either a path on local disk, an HTTP(S) URL, or an AWS S3 URL. MaxMind language support is provided by calling GetMaxMindLanguage to retrieve a user-configured language.
 func (d *MaxMindASN) Open(ctx context.Context) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	// avoids unnecessary opening
+	if d.reader != nil {
+		return nil
+	}
+
 	// language defaults to English
 	if d.Language == "" {
 		d.Language = "en"
@@ -37,7 +47,7 @@ func (d *MaxMindASN) Open(ctx context.Context) error {
 		return fmt.Errorf("database: %v", err)
 	}
 
-	if d.db, err = geoip2.Open(path); err != nil {
+	if d.reader, err = geoip2.Open(path); err != nil {
 		return fmt.Errorf("database: %v", err)
 	}
 
@@ -46,23 +56,35 @@ func (d *MaxMindASN) Open(ctx context.Context) error {
 
 // Close closes the open database.
 func (d *MaxMindASN) Close() error {
-	if d.IsEnabled() {
-		if err := d.db.Close(); err != nil {
-			return fmt.Errorf("database: %v", err)
-		}
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	// avoids unnecessary closing
+	if d.reader == nil {
+		return nil
 	}
 
+	if err := d.reader.Close(); err != nil {
+		return fmt.Errorf("database: %v", err)
+	}
+
+	// reader is made nil so that IsEnabled correctly
+	// returns the non-enabled state
+	d.reader = nil
 	return nil
 }
 
 // Get queries the database and returns an aggregated database record containing enrichment information.
 func (d *MaxMindASN) Get(addr string) (*ip.EnrichmentRecord, error) {
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+
 	paddr := net.ParseIP(addr)
 	if paddr == nil {
 		return nil, fmt.Errorf("database: %v", ip.ErrInvalidIPAddress)
 	}
 
-	resp, err := d.db.ASN(paddr)
+	resp, err := d.reader.ASN(paddr)
 	if err != nil {
 		return nil, err
 	}
@@ -81,16 +103,25 @@ func (d *MaxMindASN) Get(addr string) (*ip.EnrichmentRecord, error) {
 type MaxMindCity struct {
 	Database string `json:"database"`
 	Language string `json:"language"`
-	db       *geoip2.Reader
+	mu       sync.RWMutex
+	reader   *geoip2.Reader
 }
 
 // IsEnabled returns true if the database is open and ready for use.
 func (d *MaxMindCity) IsEnabled() bool {
-	return d.db != nil
+	return d.reader != nil
 }
 
 // Open retrieves the database and opens it for querying. MaxMind language support is provided by calling GetMaxMindLanguage to retrieve a user-configured language.
 func (d *MaxMindCity) Open(ctx context.Context) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	// avoids unnecessary opening
+	if d.reader != nil {
+		return nil
+	}
+
 	// language defaults to English
 	if d.Language == "" {
 		d.Language = "en"
@@ -103,7 +134,7 @@ func (d *MaxMindCity) Open(ctx context.Context) error {
 		return fmt.Errorf("database: %v", err)
 	}
 
-	if d.db, err = geoip2.Open(path); err != nil {
+	if d.reader, err = geoip2.Open(path); err != nil {
 		return fmt.Errorf("database: %v", err)
 	}
 
@@ -112,23 +143,35 @@ func (d *MaxMindCity) Open(ctx context.Context) error {
 
 // Close closes the open database.
 func (d *MaxMindCity) Close() error {
-	if d.IsEnabled() {
-		if err := d.db.Close(); err != nil {
-			return fmt.Errorf("database: %v", err)
-		}
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	// avoids unnecessary closing
+	if d.reader == nil {
+		return nil
 	}
 
+	if err := d.reader.Close(); err != nil {
+		return fmt.Errorf("database: %v", err)
+	}
+
+	// reader is made nil so that IsEnabled correctly
+	// returns the non-enabled state
+	d.reader = nil
 	return nil
 }
 
 // Get queries the database and returns an aggregated database record containing enrichment information.
 func (d *MaxMindCity) Get(addr string) (*ip.EnrichmentRecord, error) {
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+
 	paddr := net.ParseIP(addr)
 	if paddr == nil {
 		return nil, fmt.Errorf("database: %v", ip.ErrInvalidIPAddress)
 	}
 
-	resp, err := d.db.City(paddr)
+	resp, err := d.reader.City(paddr)
 	if err != nil {
 		return nil, fmt.Errorf("database: %v", err)
 	}

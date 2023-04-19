a40a05e3a806dc2eefbfe389ab4ed63256dfab0a
Ashish Patel <apatel@brex.com>
2023-04-19 16:40:38 -0700

    update cidr issue

--- build/terraform/aws/networking/main.tf
+++ build/terraform/aws/networking/main.tf
@@ -57,7 +57,6 @@ resource "aws_security_group" "allow_substation_tls" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {

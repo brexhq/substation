ec333e5a8a714b7e4b70f624728c52492bf0af7c
Ashish Patel <apatel@brex.com>
2023-04-20 11:42:07 -0700

    include nat gateway

--- build/terraform/aws/networking/main.tf
+++ build/terraform/aws/networking/main.tf
@@ -6,32 +6,58 @@ resource "aws_vpc" "vpc" {
 }
 
 resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = var.private_subnet_cidr
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.private_subnet_cidr
   availability_zone = var.availability_zone
-  tags = var.tags
+  tags              = var.tags
 }
 
 resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = var.public_subnet_cidr
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.public_subnet_cidr
   availability_zone = var.availability_zone
-  tags = var.tags
+  tags              = var.tags
 }
 
 resource "aws_internet_gateway" "internet_gateway" {
   vpc_id = aws_vpc.vpc.id
 }
 
-resource "aws_route_table" "public_route" {
- vpc_id = aws_vpc.vpc.id
- 
- route {
-   cidr_block = "0.0.0.0/0"
-   gateway_id = aws_internet_gateway.internet_gateway.id
- }
+resource "aws_nat_gateway" "nat_gateway" {
+  subnet_id = aws_subnet.public_subnet.id
 
- tags = var.tags
+  depends_on = [aws_internet_gateway.internet_gateway]
+}
+
+resource "aws_route_table" "public_route" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = var.tags
+}
+
+resource "aws_route_table" "private_route" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+
+  tags = var.tags
+}
+
+resource "aws_route_table_association" "private_subnet_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route.id
+
+  depends_on = [
+    aws_subnet.private_subnet,
+  ]
 }
 
 resource "aws_route_table_association" "private_subnet_association" {

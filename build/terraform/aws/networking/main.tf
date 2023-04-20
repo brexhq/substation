21a2f8d06ee79e746a2bc30d97ffbd0cfb49dbf5
Ashish Patel <apatel@brex.com>
2023-04-20 11:51:23 -0700

    update naming

--- build/terraform/aws/networking/main.tf
+++ build/terraform/aws/networking/main.tf
@@ -60,7 +60,7 @@ resource "aws_route_table_association" "private_subnet_association" {
   ]
 }
 
-resource "aws_route_table_association" "private_subnet_association" {
+resource "aws_route_table_association" "public_subnet_association" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_route.id
 

8f482dc63e06b636e0b5de46c07632e070b64826
Josh Liburdi <liburdi.joshua@gmail.com>
2023-08-03 04:56:44 +0000

    refactor: Substation Tests

--- substation_test.go
+++ substation_test.go
@@ -1,16 +1,15 @@
-package main
+package substation
 
 import (
 	"context"
 	"encoding/json"
 	"fmt"
 
-	"github.com/brexhq/substation"
 	"github.com/brexhq/substation/message"
 	"github.com/brexhq/substation/transform"
 )
 
-func main() {
+func ExampleSubstation() {
 	// Substation applications rely on a context for cancellation and timeouts.
 	ctx := context.Background()
 
@@ -26,14 +25,14 @@ func main() {
 		}
 	`)
 
-	cfg := substation.Config{}
+	cfg := Config{}
 	if err := json.Unmarshal(conf, &cfg); err != nil {
 		// Handle error.
 		panic(err)
 	}
 
 	// Create a new Substation instance.
-	sub, err := substation.New(ctx, cfg)
+	sub, err := New(ctx, cfg)
 	if err != nil {
 		// Handle error.
 		panic(err)
@@ -78,4 +77,67 @@ func main() {
 		// Handle error.
 		panic(err)
 	}
+
+	// Output:
+	// {"concurrency":-1,"transforms":[{"type":"proc_copy","settings":{"key":"a","set_key":"c"}},{"type":"send_stdout","settings":null}]}
+	// {"a":"b","c":"b"}
+}
+
+// Custom applications should embed the Substation configuration and
+// add additional configuration options.
+type customConfig struct {
+	Config
+
+	Auth struct {
+		Username string `json:"username"`
+		// Please don't store passwords in configuration files, this is only an example!
+		Password string `json:"password"`
+	} `json:"auth"`
+}
+
+// String returns an example string representation of the custom configuration.
+func (c customConfig) String() string {
+	return fmt.Sprintf("%s:%s", c.Auth.Username, c.Auth.Password)
+}
+
+func Example_customSubstation() {
+	// Substation applications rely on a context for cancellation and timeouts.
+	ctx := context.Background()
+
+	// Define and load the custom configuration. This config includes a username
+	// and password for authentication.
+	conf := []byte(`
+		{
+			"transforms":[
+				{"type":"proc_copy","settings":{"key":"a","set_key":"c"}},
+				{"type":"send_stdout"}
+			],
+			"auth":{
+				"username":"foo",
+				"password":"bar"
+			}
+		}
+	`)
+
+	cfg := customConfig{}
+	if err := json.Unmarshal(conf, &cfg); err != nil {
+		// Handle error.
+		panic(err)
+	}
+
+	// Create a new Substation instance from the embedded configuration.
+	sub, err := New(ctx, cfg.Config)
+	if err != nil {
+		// Handle error.
+		panic(err)
+	}
+
+	// Always close the Substation instance.
+	defer sub.Close(ctx)
+
+	// Print the Substation configuration.
+	fmt.Println(sub)
+
+	// Print the custom configuration.
+	fmt.Println(cfg)
 }

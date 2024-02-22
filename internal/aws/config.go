d22a2ae9cf8a441656c28560ef61cb743147431e
Josh Liburdi <jliburdi@brex.com>
2024-02-22 21:27:33 +0000

    feat(aws): Custom Retryer Support

--- internal/aws/config.go
+++ internal/aws/config.go
@@ -3,9 +3,12 @@ package aws
 import (
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/client"
 	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
+	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/aws/session"
 )
 
@@ -28,14 +31,22 @@ func New(cfg Config) (*aws.Config, *session.Session) {
 	}
 
 	if cfg.MaxRetries != 0 {
-		conf = conf.WithMaxRetries(cfg.MaxRetries)
+		conf.Retryer = CustomRetryer{
+			DefaultRetryer: client.DefaultRetryer{
+				NumMaxRetries: cfg.MaxRetries,
+			},
+		}
 	} else if v, ok := os.LookupEnv("AWS_MAX_ATTEMPTS"); ok {
 		max, err := strconv.Atoi(v)
 		if err != nil {
 			panic(err)
 		}
 
-		conf = conf.WithMaxRetries(max)
+		conf.Retryer = CustomRetryer{
+			DefaultRetryer: client.DefaultRetryer{
+				NumMaxRetries: max,
+			},
+		}
 	}
 
 	sess := session.Must(session.NewSession())
@@ -49,4 +60,18 @@ func New(cfg Config) (*aws.Config, *session.Session) {
 // NewDefault returns a new AWS configuration and session with default values.
 func NewDefault() (*aws.Config, *session.Session) {
 	return New(Config{})
+}
+
+type CustomRetryer struct {
+	client.DefaultRetryer
+}
+
+// ShouldRetry will retry if the connection is reset by the peer.
+func (r CustomRetryer) ShouldRetry(req *request.Request) bool {
+	if strings.Contains(req.Error.Error(), "read: connection reset by peer") {
+		return true
+	}
+
+	// Fallback to SDK's built in retry rules
+	return r.DefaultRetryer.ShouldRetry(req)
 }

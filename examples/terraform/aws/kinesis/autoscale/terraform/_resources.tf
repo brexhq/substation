f5dcbf5445d74b3f4132ac3f1d49edd0ff646a99
Josh Liburdi <liburdi.joshua@gmail.com>
2024-03-17 09:49:43 -0700

    docs(examples): Add Kinesis Data Stream Autoscale (#146)

---
+++ examples/terraform/aws/kinesis/autoscale/terraform/_resources.tf
@@ -0,0 +1,83 @@
+# Repository for the Autoscale app.
+module "ecr" {
+  source = "../../../../../../build/terraform/aws/ecr"
+
+  config = {
+    name         = "autoscale"
+    force_delete = true
+  }
+}
+
+# SNS topic for Kinesis Data Stream autoscale alarms.
+resource "aws_sns_topic" "autoscale" {
+  name = "autoscale"
+}
+
+# Kinesis Data Stream that is managed by the Autoscale app.
+module "kds" {
+  source = "../../../../../../build/terraform/aws/kinesis_data_stream"
+
+  config = {
+    name              = "substation"
+    autoscaling_topic = aws_sns_topic.autoscale.arn
+  }
+
+  # Min and max shards can be defined as tags to override changes made
+  # by the Autoscale app.
+  tags = {
+    MinimumShards = 2
+    MaximumShards = 4
+  }
+
+  # Add additional consumer and producer roles as needed.
+  access = [
+    # Autoscales the stream.
+    module.lambda_autoscale.role.name,
+  ]
+}
+
+# Lambda Autoscale application that manages Kinesis Data Streams.
+module "lambda_autoscale" {
+  source = "../../../../../../build/terraform/aws/lambda"
+
+  config = {
+    name        = "autoscale"
+    description = "Autoscaler for Kinesis Data Streams."
+    image_uri   = "${module.ecr.url}:latest" # This should use the project's release tags.
+    image_arm   = true
+
+    # Override the default Autoscale configuration using environment variables.
+    # These are the default settings, included for demonstration purposes.
+    env = {
+      "AUTOSCALE_KINESIS_THRESHOLD" : 0.7,
+      "AUTOSCALE_KINESIS_UPSCALE_DATAPOINTS" : 5,
+      "AUTOSCALE_KINESIS_DOWNSCALE_DATAPOINTS" : 60,
+    }
+  }
+
+  depends_on = [
+    module.ecr.url,
+  ]
+}
+
+resource "aws_sns_topic_subscription" "autoscale_subscription" {
+  topic_arn = aws_sns_topic.autoscale.arn
+  protocol  = "lambda"
+  endpoint  = module.lambda_autoscale.arn
+
+  depends_on = [
+    module.lambda_autoscale.name
+  ]
+}
+
+resource "aws_lambda_permission" "autoscale_invoke" {
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = module.lambda_autoscale.name
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.autoscale.arn
+
+  depends_on = [
+    module.lambda_autoscale.name
+  ]
+}

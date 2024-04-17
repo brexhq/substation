0d7c9973be550244b35f7f3a1a103a3409ffce77
jshlbrd <jliburdi@brex.com>
2024-04-17 06:20:51 +0000

    docs(examples): Update Kinesis Time Travel Example

--- examples/terraform/aws/kinesis/time_travel/config/const.libsonnet
+++ examples/terraform/aws/kinesis/time_travel/config/const.libsonnet
@@ -1,10 +1,12 @@
 local sub = import '../../../../../../build/config/substation.libsonnet';
 
 {
+  is_process: [
+    sub.cnd.str.eq({ obj: { src: 'event.category' }, value: 'process' }),
+    sub.cnd.str.eq({ obj: { src: 'event.type' }, value: 'start' }),
+  ],
   kv_store: sub.kv_store.aws_dynamodb({
     table_name: 'substation',
     attributes: { partition_key: 'PK', sort_key: 'SK', ttl: 'TTL', value: 'cache' },
   }),
-  field: 'context',
-  field_exists: sub.cnd.num.len.gt({ obj: { src: $.field }, value: 0 }),
 }
--- examples/terraform/aws/kinesis/time_travel/terraform/_resources.tf
+++ examples/terraform/aws/kinesis/time_travel/terraform/_resources.tf
@@ -1,68 +1,15 @@
-data "aws_caller_identity" "caller" {}
-
-# KMS encryption key that is shared by all Substation infrastructure
-module "kms" {
-  source = "../../../../../../build/terraform/aws/kms"
-
-  config = {
-    name   = "alias/substation"
-    policy = data.aws_iam_policy_document.kms.json
-  }
-}
-
-# This policy is required to support encrypted SNS topics.
-# More information: https://repost.aws/knowledge-center/cloudwatch-receive-sns-for-alarm-trigger
-data "aws_iam_policy_document" "kms" {
-  # Allows CloudWatch to access encrypted SNS topic.
-  statement {
-    sid    = "CloudWatch"
-    effect = "Allow"
-    actions = [
-      "kms:Decrypt",
-      "kms:GenerateDataKey"
-    ]
-
-    principals {
-      type        = "Service"
-      identifiers = ["cloudwatch.amazonaws.com"]
-    }
-
-    resources = ["*"]
-  }
-
-  # Default key policy for KMS.
-  # https://docs.aws.amazon.com/kms/latest/developerguide/determining-access-key-policy.html
-  statement {
-    sid    = "KMS"
-    effect = "Allow"
-    actions = [
-      "kms:*",
-    ]
-
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::${data.aws_caller_identity.caller.account_id}:root"]
-    }
-
-    resources = ["*"]
-  }
-}
-
 module "appconfig" {
   source = "../../../../../../build/terraform/aws/appconfig"
 
   config = {
-    name = "substation"
-    environments = [{
-      name = "example"
-    }]
+    name         = "substation"
+    environments = [{ name = "example" }]
   }
 }
 
 # Repository for the core Substation application.
 module "ecr" {
   source = "../../../../../../build/terraform/aws/ecr"
-  kms    = module.kms
 
   config = {
     name         = "substation"
@@ -73,7 +20,6 @@ module "ecr" {
 # Repository for the autoscaling application.
 module "ecr_autoscale" {
   source = "../../../../../../build/terraform/aws/ecr"
-  kms    = module.kms
 
   config = {
     name         = "autoscale"
@@ -83,25 +29,12 @@ module "ecr_autoscale" {
 
 # SNS topic for Kinesis Data Stream autoscaling alarms.
 resource "aws_sns_topic" "autoscaling_topic" {
-  name              = "autoscale"
-  kms_master_key_id = module.kms.id
-}
-
-# API Gateway that sends data to Kinesis.
-module "gateway" {
-  source = "../../../../../../build/terraform/aws/api_gateway/kinesis_data_stream"
-  # Always required for the Kinisis Data Stream integration.
-  kinesis_data_stream = module.kinesis
-
-  config = {
-    name = "gateway"
-  }
+  name = "autoscale"
 }
 
 # Kinesis Data Stream that stores data sent from pipeline sources.
 module "kinesis" {
   source = "../../../../../../build/terraform/aws/kinesis_data_stream"
-  kms    = module.kms
 
   config = {
     name              = "substation"
@@ -113,15 +46,12 @@ module "kinesis" {
     module.lambda_autoscaling.role.name,
     # Consumes data from the stream.
     module.lambda_enrichment.role.name,
-    module.lambda_subscriber.role.name,
-    # Publishes data to the stream.
-    module.gateway.role.name,
+    module.lambda_transform.role.name,
   ]
 }
 
 module "dynamodb" {
   source = "../../../../../../build/terraform/aws/dynamodb"
-  kms    = module.kms
 
   config = {
     name      = "substation"
@@ -143,6 +73,6 @@ module "dynamodb" {
 
   access = [
     module.lambda_enrichment.role.name,
-    module.lambda_subscriber.role.name,
+    module.lambda_transform.role.name,
   ]
 }

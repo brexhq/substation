d6b6c429ccbe341a041a609dc778785b004eb1e7
Josh Liburdi <liburdi.joshua@gmail.com>
2024-06-06 10:09:33 -0700

    fix(metrics): AWS CloudWatch Embedded Metrics Dimensions (#179)

--- internal/metrics/aws_cloudwatch_embedded_metrics.go
+++ internal/metrics/aws_cloudwatch_embedded_metrics.go
@@ -45,16 +45,19 @@ func (m *awsCloudWatchEmbeddedMetrics) Generate(ctx context.Context, data Data) 
 		return fmt.Errorf("metrics log_embedded_metrics: %v", err)
 	}
 
+	var dimensions []string
 	for key, val := range data.Attributes {
-		emf, err = sjson.SetBytes(emf, "_aws.CloudWatchMetrics.0.Dimensions.-1.-1", key)
-		if err != nil {
-			return fmt.Errorf("metrics log_embedded_metrics: %v", err)
-		}
+		dimensions = append(dimensions, key)
 
 		emf, err = sjson.SetBytes(emf, key, val)
 		if err != nil {
 			return fmt.Errorf("metrics log_embedded_metrics: %v", err)
 		}
+	}
+
+	emf, err = sjson.SetBytes(emf, "_aws.CloudWatchMetrics.0.Dimensions.-1", dimensions)
+	if err != nil {
+		return fmt.Errorf("metrics log_embedded_metrics: %v", err)
 	}
 
 	emf, err = sjson.SetBytes(emf, "_aws.CloudWatchMetrics.0.Metrics.0.Name", data.Name)

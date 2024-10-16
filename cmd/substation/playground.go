5d40576c3e8abb6dbda66873720a76c15d01f24d
brittonhayes <brittonhayes@users.noreply.github.com>
2024-10-16 12:14:19 -0700

    feat(playground): ux improvements, share button, demo, improved copy

--- cmd/substation/playground.go
+++ cmd/substation/playground.go
@@ -10,10 +10,10 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/spf13/cobra"
-	"github.com/tidwall/gjson"
 
 	"github.com/brexhq/substation/v2"
 	"github.com/brexhq/substation/v2/message"
@@ -39,8 +39,8 @@ func runPlayground(cmd *cobra.Command, args []string) error {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handleIndex)
 	mux.HandleFunc("/run", handleRun)
+	mux.HandleFunc("/demo", handleDemo)
 	mux.HandleFunc("/fmt", handleFmt)
-	mux.HandleFunc("/examples", handleExamples)
 
 	server := &http.Server{
 		Addr:    ":8080",
@@ -67,13 +67,27 @@ func handleIndex(w http.ResponseWriter, r *http.Request) {
 		DefaultConfig string
 		DefaultInput  string
 	}{
-		DefaultConfig: demoConf,
-		DefaultInput:  demoEvt,
+		DefaultConfig: "",
+		DefaultInput:  "",
 	}
+
 	tmpl := template.Must(template.New("index").Parse(indexHTML))
 	if err := tmpl.Execute(w, data); err != nil {
 		log.Printf("Error executing template: %v", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
+func handleDemo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	cleanedDemoconf := strings.ReplaceAll(demoConf, "local sub = import '../../substation.libsonnet';\n\n", "")
+
+	if err := json.NewEncoder(w).Encode(map[string]interface{}{
+		"config": cleanedDemoconf,
+		"input":  demoEvt,
+	}); err != nil {
+		http.Error(w, fmt.Sprintf("Error encoding response: %v", err), http.StatusInternalServerError)
 	}
 }
 
@@ -130,54 +144,13 @@ func handleRun(w http.ResponseWriter, r *http.Request) {
 	var output []string
 	for _, msg := range result {
 		if !msg.IsControl() {
-			output = append(output, gjson.Get(string(msg.Data()), "@this|@pretty").String())
+			output = append(output, string(msg.Data()))
 		}
 	}
 
 	if err := json.NewEncoder(w).Encode(map[string]interface{}{
 		"output": output,
 	}); err != nil {
-		http.Error(w, fmt.Sprintf("Error encoding response: %v", err), http.StatusInternalServerError)
-	}
-}
-
-func handleExamples(w http.ResponseWriter, r *http.Request) {
-	examples := map[string]struct {
-		Config string `json:"config"`
-		Input  string `json:"input"`
-	}{
-		"stringConversion": {
-			Config: `{
-  transforms: [
-    sub.tf.time.from.string({ obj: { source_key: 'time', target_key: 'time' }, format: '2006-01-02T15:04:05.000Z' }),
-    sub.tf.time.to.string({ obj: { source_key: 'time', target_key: 'time' }, format: '2006-01-02T15:04:05' }),
-  ],
-}`,
-			Input: `{"time":"2024-01-01T01:02:03.123Z"}`,
-		},
-		"numberClamp": {
-			Config: `{
-  transforms: [
-    sub.tf.number.maximum({ value: 0 }),
-    sub.tf.number.minimum({ value: 100 }),
-  ],
-}`,
-			Input: `-1
-101
-50`,
-		},
-		"arrayFlatten": {
-			Config: `{
-  transforms: [
-    sub.tf.obj.cp({ object: { source_key: 'a|@flatten', target_key: 'a' } }),
-    sub.tf.obj.cp({ object: { source_key: '@pretty' } }),
-  ],
-}`,
-			Input: `{"a":[1,2,[3,4]]}`,
-		},
-	}
-
-	if err := json.NewEncoder(w).Encode(examples); err != nil {
 		http.Error(w, fmt.Sprintf("Error encoding response: %v", err), http.StatusInternalServerError)
 	}
 }
@@ -232,75 +205,97 @@ const indexHTML = `
 
         body {
             font-family: 'Inter', sans-serif;
-            max-width: 90vw;
-            margin: 0 auto;
-            padding: 40px;
+            margin: 0;
+            padding: 0;
             background-color: #f9f9f9;
             color: var(--text-color);
-            display: grid;
-            grid-template-rows: auto 1fr;
-            height: 100vh;
+            display: flex;
+            flex-direction: column;
+            min-height: 95vh;
             box-sizing: border-box;
         }
 
-        header {
-            margin-bottom: 40px;
+        .content-wrapper {
+            margin: 0 auto;
+            padding: 0 40px;
+            width: 100%;
+            box-sizing: border-box;
+        }
+
+        .nav-bar {
+            background-color: #ffffff;
+            padding: 10px 0;
+            box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+        }
+
+        .nav-content {
             display: flex;
             justify-content: space-between;
-            align-items: flex-start;
+            align-items: center;
         }
 
         .title {
-            gap: 16px;
-        }
-
-        .title-container {
-            position: relative;
-            padding-bottom: 1em;
-        }
-
-        h1 {
-            font-size: 48px;
-            color: #212121;
+            font-size: 20px;
             font-weight: 800;
-            margin-bottom: 8px;
-            word-wrap: break-word;
+            color: #212121;
         }
 
         .playground-label {
             font-weight: 300;
-            font-family: 'Inter', sans-serif;
             color: var(--secondary-color);
             opacity: 0.5;
         }
 
-        h2 {
-            font-size: 24px;
-            color: #202020;
-            font-weight: 700;
-            margin-top: 0;
-            margin-bottom: 4px;
+        .nav-links {
+            display: flex;
+            gap: 20px;
         }
 
-        h3 {
-            font-weight: 500;
-            color: #666666;
-            font-size: 18px;
-            margin-top: 0;
+        .nav-link {
+            color: var(--secondary-color);
+            text-decoration: none;
+            font-size: 20px;
+            transition: color 0.3s ease;
+        }
+
+        .nav-link:hover {
+            color: var(--secondary-hover-color);
+        }
+
+        .action-section {
+            padding: 20px 0;
+            background-color: #f0f0f04e;
+            border-bottom: 1px solid var(--border-color);
+        }
+
+        .button-container {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            gap: 10px;
+        }
+
+        .action-row {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            gap: 10px;
         }
 
         main {
             display: grid;
             grid-template-columns: 1fr 1fr;
             gap: 40px;
-            height: 100%;
+            flex-grow: 1;
+            overflow: hidden;
+            padding: 20px 0;
         }
 
         .left-column,
         .right-column {
             display: flex;
             flex-direction: column;
-            gap: 20px;
+            gap: 18px;
             overflow: hidden;
         }
 
@@ -323,42 +318,17 @@ const indexHTML = `
             overflow: hidden;
         }
 
-        .button-container {
-            display: flex;
-            flex-direction: column;
-            align-items: flex-start;
-            gap: 5px;
-        }
-
-        .action-row {
-            display: flex;
-            flex-direction: row;
-            align-items: center;
-            gap: 10px;
-        }
-
-        .select-container {
-            display: flex;
-            flex-direction: column;
-        }
-
-        .select-container select {
-            height: 40px;
-            box-sizing: border-box;
-            padding: 0 10px;
-        }
-
         .subtext {
             font-size: 12px;
             color: var(--secondary-color);
-            margin: 5px 0 8px 0;  // Added bottom margin
+            margin: 5px 0 8px 0; 
         }
 
         button {
-            padding: 0 48px;
-            height: 40px;
+            padding: 0 24px;
+            height: 36px;
             color: white;
-            border: none;
+            border: 1px solid var(--border-color);
             border-radius: 8px;
             cursor: pointer;
             font-family: 'Inter', sans-serif;
@@ -376,91 +346,55 @@ const indexHTML = `
             background-color: var(--primary-hover-color);
         }
 
-        .format-button {
+        .secondary-button {
             background-color: #EDEFEE;
             color: #323333;
         }
 
-        .format-button:hover {
+        .secondary-button:hover {
             background-color: #D9DBD9;
-        }
-
-        .secondary-link {
-            color: var(--secondary-color);
-            text-decoration: none;
-            font-weight: 500;
         }
 
         button:active {
             transform: translateY(1px);
         }
 
-        @media (max-width: 1200px) {
-            body {
-                max-width: 95vw;
-            }
+        .examples-link {
+            color: var(--primary-color);
+            text-decoration: none;
+        }
 
+        .examples-link:hover {
+            text-decoration: underline;
+        }
+
+        @media (max-width: 1200px) {
             main {
                 grid-template-columns: 1fr;
             }
-
-            h1 {
-                font-size: 36px;
-            }
-
-            h3 {
-                font-size: 16px;
+            .content-wrapper {
+                padding: 0 20px;
             }
         }
 
+        h2 {
+            margin: 24px 0px 0px 0px
+        }
 
-        .nav-bar {
-            position: absolute;
-            top: 20px;
-            right: 40px;
+        .editor-header {
             display: flex;
-            gap: 20px;
+            justify-content: space-between;
+            align-items: center;
+            margin-bottom: 4px;
         }
 
-        .nav-link {
-            color: var(--secondary-color);
-            text-decoration: none;
-            font-size: 24px;
-            transition: color 0.3s ease;
-        }
-
-        .nav-link:hover {
-            color: var(--secondary-hover-color);
-        }
-
-        select {
-            padding: 10px;
-            font-size: 16px;
-            border: 1px solid var(--border-color);
+        .mode-selector {
+            font-size: 14px;
+            padding: 5px;
             border-radius: 4px;
+            border: 1px solid var(--border-color);
             background-color: #ffffff;
             color: var(--text-color);
-            cursor: pointer;
-        }
-
-        select:hover {
-            border-color: var(--primary-color);
-        }
-
-        .logo {
-            height: 36px;
-            width: auto;
-        }
-
-        .title {
-            display: flex;
-            align-items: center;
-        }
-
-        .logo-container {
-            position: absolute;
-            top: 20px;
-            left: 40px;
         }
     </style>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.30.1/min/vs/loader.min.js"></script>
@@ -469,56 +403,70 @@ const indexHTML = `
 </head>
 
 <body>
-    <header>
-        <div class="nav-bar">
-            <a href="https://substation.readme.io/docs/overview" target="_blank" class="nav-link" title="Documentation">
-                <i class="fas fa-book"></i>
-            </a>
-            <a href="https://github.com/brexhq/substation" target="_blank" class="nav-link" title="GitHub">
-                <i class="fab fa-github"></i>
-            </a>
-        </div>
-        <div>
-            <div class="title-container">
-                <h1 class="title">
-                    <span>Substation</span>
-                    <span class="playground-label">Playground</span>
-                </h1>
-                <h3>A toolkit for routing, normalizing, and enriching security event and audit logs.</h3>
+    <nav class="nav-bar">
+        <div class="content-wrapper">
+            <div class="nav-content">
+                <div class="title">
+                    Substation <span class="playground-label">Playground</span>
+                </div>
+                <div class="nav-links">
+                    <a href="https://substation.readme.io/docs/overview" target="_blank" class="nav-link" title="Documentation">
+                        <i class="fas fa-book"></i>
+                    </a>
+                    <a href="https://github.com/brexhq/substation" target="_blank" class="nav-link" title="GitHub">
+                        <i class="fab fa-github"></i>
+                    </a>
+                </div>
             </div>
+        </div>
+    </nav>
+    <section class="action-section">
+        <div class="content-wrapper">
             <div class="button-container">
                 <div class="action-row">
-                    <div class="select-container">
-                        <select id="exampleSelector" onchange="loadExample()">
-                            <option value="stringConversion">String Conversion</option>
-                            <option value="numberClamp">Number Clamp</option>
-                            <option value="arrayFlatten">Array Flatten</option>
-                        </select>
-                    </div>
                     <button class="primary-button" onclick="runSubstation()">Run</button>
-                    <button class="format-button" onclick="formatJsonnet()">Format</button>
+                    <button class="secondary-button" onclick="testSubstation()">Test</button>
+                    <button class="secondary-button" onclick="demoSubstation()">Demo</button>
+                    <button class="secondary-button" onclick="formatJsonnet()">Format</button>
                 </div>
-                <p class="subtext">Select an example to get started or create your own configuration.</p>
+                <p class="subtext">
+                    Run your configuration, test it, or try a demo. 
+                    <a href="https://github.com/brexhq/substation/tree/main/examples" target="_blank" class="examples-link">View examples</a>
+                </p>
             </div>
         </div>
-    </header>
-    <main>
+    </section>
+    <main class="content-wrapper">
         <section class="left-column">
             <div class="editor-section">
-                <h2>Configuration</h2>
+                <div class="editor-header">
+                    <h2>Configuration</h2>
+                </div>
                 <p class="subtext">Configure the transformations to be applied to the input event.</p>
                 <div class="editor-container" id="config"></div>
             </div>
         </section>
         <section class="right-column">
             <div class="editor-section">
-                <h2>Input</h2>
-                <p class="subtext">Paste the JSON event to be processed by Substation here.</p>
+                <div class="editor-header">
+                    <h2>Input</h2>
+                    <select class="mode-selector" id="inputModeSelector" onchange="changeEditorMode('input')">
+                        <option value="text">Text</option>
+                        <option value="json">JSON</option>
+                    </select>
+                </div>
+                <p class="subtext">Paste the message data to be processed by Substation here.</p>
                 <div class="editor-container" id="input"></div>
             </div>
             <div class="editor-section">
-                <h2>Output</h2>
-                <p class="subtext">The processed event will appear here after running.</p>
+                <div class="editor-header">
+                    <h2>Output</h2>
+                    <select class="mode-selector" id="outputModeSelector" onchange="changeEditorMode('output')">
+                        <option value="text">Text</option>
+                        <option value="json">JSON</option>
+                    </select>
+                </div>
+                <p class="subtext">The processed message data will appear here after running.</p>
                 <div class="editor-container" id="output"></div>
             </div>
         </section>
@@ -543,25 +491,22 @@ const indexHTML = `
                     roundedSelection: false,
                     readOnly: elementId === 'output',
                     renderLineHighlight: 'none',
+                    wordWrap: 'on', 
                 });
             }
 
-
             configEditor = createEditor('config', 'jsonnet', "");
-            inputEditor = createEditor('input', 'json', "");
-            outputEditor = createEditor('output', 'json', '// Output will appear here');
-
-            // Fetch examples from the API and set default example
-            fetch('/examples')
-                .then(response => response.json())
-                .then(data => {
-                    examples = data;
-                    // Set the default example to "stringConversion"
-                    document.getElementById('exampleSelector').value = 'stringConversion';
-                    loadExample();
-                })
-                .catch(error => console.error('Error fetching examples:', error));
+            inputEditor = createEditor('input', 'text', "");
+            outputEditor = createEditor('output', 'text', '// Processed message data will appear here');
         });
+
+        function changeEditorMode(editorId) {
+            const editor = editorId === 'input' ? inputEditor : outputEditor;
+            const id = editorId + "ModeSelector";
+            const selector = document.getElementById(id);
+            const newModel = monaco.editor.createModel(editor.getValue(), selector.value);
+            editor.setModel(newModel);
+        }
 
         function runSubstation() {
             fetch('/run', {
@@ -569,7 +514,7 @@ const indexHTML = `
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
                     config: configEditor.getValue(),
-                    input: inputEditor.getValue()
+                    input: inputEditor.getValue(),
                 })
             })
                 .then(response => response.json())
@@ -594,17 +539,35 @@ const indexHTML = `
                 .catch(error => console.error('Error formatting Jsonnet:', error));
         }
 
-        function loadExample() {
-            const example = document.getElementById('exampleSelector').value;
-            if (example in examples) {
-                configEditor.setValue(examples[example].config);
-                inputEditor.setValue(examples[example].input);
-                outputEditor.setValue('// Output will appear here');
-            } else if (example === '') {
-                configEditor.setValue("");
-                inputEditor.setValue("");
-                outputEditor.setValue('// Output will appear here');
-            }
+        function testSubstation() {
+            fetch('/test', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    config: configEditor.getValue(),
+                    input: inputEditor.getValue(),
+                })
+            })
+            .then(response => response.json())
+            .then(data => {
+                outputEditor.setValue(JSON.stringify(data, null, 2));
+            })
+            .catch(error => {
+                outputEditor.setValue('Error: ' + error);
+            });
+        }
+
+        function demoSubstation() {
+            fetch('/demo')
+            .then(response => response.json())
+            .then(data => {
+                configEditor.setValue(data.config);
+                inputEditor.setValue(data.input);
+                outputEditor.setValue('// Run the demo to see the output');
+            })
+            .catch(error => {
+                console.error('Error fetching demo:', error);
+            });
         }
     </script>
 </body>

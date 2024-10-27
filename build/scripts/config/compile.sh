df35d4c657feaa76241850419944774580bd0f4d
jshlbrd <liburdi.joshua@gmail.com>
2024-10-27 09:50:43 -0700

    chore: Delete Scripts, Commands

--- build/scripts/config/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-files=$(find . -name "*.jsonnet")
-
-for file in $files
-do
-  # 'rev | cut | rev' converts "path/to/file.jsonnet" to "path/to/file.json"
-  f=$(echo $file | rev | cut -c 4- | rev)
-  jsonnet $file > $f
-done
